462d1d9017e93e1101ae635128c96ea8d493f878
hee c <valentin1679918@gmail.com>
2022-04-05 23:12:50 +0900

    add_key at hashmap.c edited

--- hashmap.c
+++ hashmap.c
@@ -3,6 +3,9 @@
 #include "node.h"
 #include "hashmap.h"
 
+#define GET_INDEX_BY_KEY(key) (hashmap->hash_func(key) % hashmap->length);
+
+
 static node_t* get_node_or_null(node_t** phead, const char* str)
 {
     node_t** pp = phead;
@@ -31,40 +34,32 @@ hashmap_t* init_hashmap_malloc(size_t length, size_t (*p_hash_func)(const char* 
 
 int add_key(hashmap_t* hashmap, const char* key, const int value)
 {
-    unsigned int hashed = hashmap->hash_func(key) % hashmap->length;
-    node_t* pa_node;
-    node_t* cur_node;
+    node_t* new_node;
+    node_t** phead;
+
+    unsigned int hashed = GET_INDEX_BY_KEY(key);
 
     if (strcmp(key, "") == 0) {
         return FALSE;
     }
 
-    pa_node = malloc(sizeof(node_t));
-    pa_node->key = malloc(strlen(key) + 1);
-    strcpy(pa_node->key, key);
-    pa_node->value = value;
-    pa_node->next = NULL;
-
-    /* 1. 해시테이블에 매핑된 값이 없을 때 */
-    if (hashmap->plist[hashed] == NULL) {
-        hashmap->plist[hashed] = pa_node;
-        return TRUE;
-    }
-
-    /* 2. 해시테이블에 매핑된 값이 있고, 지금 삽입하려는 키가 원래있는 키랑 같지않을때만 새로운 노드를 삽입 */
-    cur_node = hashmap->plist[hashed];
-    while (cur_node != NULL) {
-        if (cur_node->next == NULL && strcmp(cur_node->key, key) != 0) {
-            cur_node->next = pa_node;
-            return TRUE;
+    phead = &(hashmap->plist[hashed]);
+    while (*phead != NULL) {
+        if (strcmp((*phead)->key, key) == 0) {
+            return FALSE;
         }
-        cur_node = cur_node->next;
+        phead = &(*phead)->next;
     }
-    
-    free(pa_node->key);
-    free(pa_node);
 
-    return FALSE;
+    new_node = malloc(sizeof(node_t));
+    new_node->key = malloc(strlen(key) + 1);
+    strcpy(new_node->key, key);
+    new_node->value = value;
+    new_node->next = NULL;
+
+    *phead = new_node;
+
+    return TRUE;
 }
 
 int get_value(const hashmap_t* hashmap, const char* key)
