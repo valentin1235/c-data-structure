bf394ef20359f531751f81ed9c35e2c9558a330f
hee c <valentin1679918@gmail.com>
2022-05-24 23:51:47 +0900

    wip

--- src/main.c
+++ src/main.c
@@ -3,10 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
-// #include "stack.h"
-// #include "queue.h"
-// #include "linkedlist.h"
-// #include "hashmap.h"
 #include "../header/data_structure.h"
 
 #define DEFAULT_ARRAY_LENGTH (20)
